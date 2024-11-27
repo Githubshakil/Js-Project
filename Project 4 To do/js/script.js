ae11c8a37027613c557f8fff10a4e02598239f65
Githubshakil <shakilmahmud007@gmail.com>
2024-11-27 19:01:28 +0600

    update

--- Project 4 To do/js/script.js
+++ Project 4 To do/js/script.js
@@ -2,7 +2,7 @@ let input = document.querySelector("input");
 let postbutton = document.querySelector(".post_btn");
 let textoutput = document.querySelector(".text_output");
 let editbutton = document.querySelector(".edit_btn") 
-let clearbutton = document.querySelector(".clear_btn") 
+ 
 let taskArr = [];
 
 
@@ -13,16 +13,38 @@ let taskArr = [];
 
 postbutton.addEventListener("click", function () {
   taskArr.push(input.value);
-  textoutput.innerHTML = "";
-  taskArr.map((task) => {
-    textoutput.innerHTML += `<p>${task}</p>`;
-  });
+  displayUpdate()
+
 
   input.value = "";
 });
-
-    clearbutton.addEventListener("click", function(){
+function displayUpdate() {
     textoutput.innerHTML = "";
-    input.value = "";
-    
+    taskArr.map((task) => {
+      textoutput.innerHTML += `<p> ${task}</p>`;
+    });
+
+
+    let clearbutton = document.querySelectorAll(".clear_btn")
+
+    let clearArr = Array.from(clearbutton)
+
+  
+  clearArr.map((clear,) => {
+    clear.addEventListener("click", function(){
+        
+        console.log("clearbutton");
+        taskArr.splice(0,1)
+        displayUpdate()
+        console.log(taskArr);
+        
+
 })
+  })
+}
+
+
+
+
+
+
