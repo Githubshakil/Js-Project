cd98619703b61f0697d300442d0267882eb054d2
Githubshakil <shakilmahmud007@gmail.com>
2024-12-28 02:06:42 +0600

    update

--- jobpost2/js/script.js
+++ jobpost2/js/script.js
@@ -1,43 +1,36 @@
-
 let employees = [
-    { id: 1, name: 'Alice', tasksCompleted: 50, rating: 4.8 },
-    { id: 2, name: 'Bob', tasksCompleted: 30, rating: 3.9 },
-    { id: 3, name: 'Charlie', tasksCompleted: 70, rating: 4.5 },
-    { id: 4, name: 'Diana', tasksCompleted: 20, rating: 3.2 },
+  { id: 1, name: "Alice", tasksCompleted: 50, rating: 4.8 },
+  { id: 2, name: "Bob", tasksCompleted: 30, rating: 3.9 },
+  { id: 3, name: "Charlie", tasksCompleted: 70, rating: 4.5 },
+  { id: 4, name: "Diana", tasksCompleted: 20, rating: 3.2 },
 ];
 
+// function calculate bonuse
+function calculateBonuses(employees) {
+  let results = [];
+//   console.log(employees);
 
-
-
-//function calculate bonuse
-function calculateBonuses(employees){
-    let results=[];
-
-
-    //loop array of employe
-    for( let i = 0; i < employees.lenght; i++){
-        let employee = employees[i];
+  //loop array of employe
+  for (let i = 0; i < employees.length; i++) {
+    let employee = employees[i];
     
-    
+
     //employee bonuse
-
-    if(employee.tasksCompleted >= 40 && employee.rating >= 4.0){
-        let bonus = employee.tasksCompleted * 10;
-
-        results.push({
-            id:employee.id,
-            name:employee.name,
-            bonus:bonus
-        });
+    
+    if (employee.tasksCompleted >= 40 && employee.rating >= 4.0) {
+      let bonus = employee.tasksCompleted * 10;
+      results.push({
+        id: employee.id,
+        name: employee.name,
+        bonus: bonus,
+      });
     }
-}
-    return results;
+  }
+
+//   console.log(results);
+  return results;
 }
 let bonuses = calculateBonuses(employees);
 
-
 console.log("employee bonuses:", bonuses);
 
-
-
-  
