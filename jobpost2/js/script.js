d2ebe0d02f1b3d221a8586b48aa644e3b76b5149
Githubshakil <shakilmahmud007@gmail.com>
2024-12-29 22:12:59 +0600

    update

--- jobpost2/js/script.js
+++ jobpost2/js/script.js
@@ -1,8 +1,8 @@
 let employees = [
-  { id: 1, name: "Alice", tasksCompleted: 50, rating: 4.8 },
+  { id: 1, name: "Alice", tasksCompleted: 20, rating: 3.2 },
   { id: 2, name: "Bob", tasksCompleted: 30, rating: 3.9 },
   { id: 3, name: "Charlie", tasksCompleted: 70, rating: 4.5 },
-  { id: 4, name: "Diana", tasksCompleted: 20, rating: 3.2 },
+  { id: 4, name: "Diana", tasksCompleted: 50, rating: 4.8 },
 ];
 
 // function calculate bonuse
--- jobpost3/js/script.js
+++ jobpost3/js/script.js
@@ -1,6 +1,6 @@
 //students array
 const students = [
-  { id: 1, name: "John", scores: [85, 78, 92] },
+  { id: 1, name: "John", scores: [50, 48, 55] },
   { id: 2, name: "Sara", scores: [62, 70, 68] },
   { id: 3, name: "Emma", scores: [90, 95, 94] },
   { id: 4, name: "Tom", scores: [50, 48, 55] },
